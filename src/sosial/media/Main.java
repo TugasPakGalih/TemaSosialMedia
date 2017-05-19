061d95bd58490ae770e4c401d5505f7d1a4cad2e
Dwiky11 <dwiky.s98@gmail.com>
2017-05-19 20:43:41 +0700

    melengkapi class main(Dwiky Setiawan)

--- src/sosial/media/Main.java
+++ src/sosial/media/Main.java
@@ -29,5 +29,25 @@ public class Main {
         String tanggalLahir = new Scanner(System.in).nextLine();
         System.out.print("Hobby\t\t: "); 
         String hobby = new Scanner(System.in).nextLine();
+        people.add(new Data("Yoga Pamungkas","Laki-Laki","Teknik Informatika","12 Januari 1996","Main Gitar"));
+        people.add(new Data("Muhamad Ridwan","Laki-Laki","Akuntasni","25 Juni 1997","Membaca"));
+        people.add(new Data("Galus Panji","Laki-Laki","Sosiologi","1 Agustus 1998","Basket"));
+        people.add(new Data("Hilman","Laki-Laki","Teknik Industri","15 Juli 1997","Karate"));
+        people.add(new Data("Khoerunisa","Perempuan","Ilmu Pemerintahan","5 Maret1998","Shopping"));
+        people.add(new Data("Alfian","Laki-Laki","Akuntansi","1 Mei 1997","Main Game"));
+        people.add(new Data(namaLengkap, jenisKelamin, jurusan, tanggalLahir, hobby));
+
+        ArrayList<Data> pilihOrang = new ArrayList<Data>();
+        char pilih='y';
+        int i=1;
+        int pilihIndeks=1;
+        while (pilih == 'y'){
+            System.out.println("\n===========Daftar Semua User============");
+            for (Data b:people
+                    ) {
+                System.out.println(i+" "+b.getNamaLengkap());
+                i++;
+            }
+        
 }
-}
+    }}
